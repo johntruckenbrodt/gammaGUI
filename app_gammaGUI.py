794c1feba1f40b4582eea5c97f99a3c6f89b20b9
Matisyahu18 <stefan.werner@uni-jena.de>
2019-03-29 18:18:08 +0100

    made it pretty

--- app_gammaGUI.py
+++ app_gammaGUI.py
@@ -1,9 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from gammaGUIv2.gui_windows.QMainWindow import Ui_MainWindow
-#from gammaGUIv2.gui_windows.QBrowseDialoge import BrowseDialoge
-# from gammaGUIv2.QSubwindow_default import Ui_QSubwindow_default
-# from auxiliary import environment
 
 def main():
     """
