ded35deca1bfff8d4be9b9467b380a813204b585
Matisyahu18 <stefan.werner@uni-jena.de>
2019-03-29 16:08:30 +0100

    made it pretty

--- gammaGUIv2/gui_windows/QSubWindow_data_extraction.py
+++ gammaGUIv2/gui_windows/QSubWindow_data_extraction.py
@@ -13,22 +13,23 @@ from auxilliary.extract_zip import ZippedFiles
 
 class Ui_QSubWindow_data_extraction(object):
     """
-    This is the Class of the Data Extraction Window. It contains the setupUI, retranslateUi, create_Ui_QSubWindow_data_extraction Methods
-    From the Main Window the create_Ui_QSubWindow_data_extraction Method is called to open the Window, within this call
-    setupUI, retranslateUi are called to initialize the Window.
-    """
+    This is the QSubWindow Class of the Date Extraction Window. This Class contains the hole Layout of the QSubWindow
+    including all Buttons witch can be occupied with Functionality.
+    Here you can link the Button with Function via accepted.connect() or clicked.connect():
+        - writing the WorkEnv.xml for Input and Output
+        - starting a Script when pressing ok eg. extraction zips
+   """
     def setupUi(self, QSubWindow_data_extraction):
         """
-        This is the Method to setup the Window.
-        Here are the changes made, to add functionality to the Window. Examples are given in gammaGUIv2/gui_windows/QSubWindow_set_your_working_directory.py
-        -----
+        This the the Setup Method for the UI.
         TODO Add functionality to ALL Buttons -> further details for the TODO's see below
-
         :param QSubWindow_data_extraction:
         :return:
         """
 
-        # Window Layout
+        ###
+        # Setup for QSubwindow_{}
+        ###
         QSubWindow_data_extraction.setObjectName("QSubWindow_data_extraction")
         QSubWindow_data_extraction.resize(685, 255)
         self.verticalLayout = QtWidgets.QVBoxLayout(QSubWindow_data_extraction)
@@ -42,13 +43,15 @@ class Ui_QSubWindow_data_extraction(object):
         spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_8.addItem(spacerItem)
 
-        #### CheckBOX: Select all Files ####
+        ###
+        # CheckBOX: Select all Files
+        ###
         #TODO
         # Write and implement a Method to read this Checkbox and automatically select all Files in Folder, when pressing OK.
         # I think this has to be done with Signals and in the auxilliary/extract_zip.py, If statement
         self.checkBox_3 = QtWidgets.QCheckBox(self.horizontalWidget_8)
         self.checkBox_3.setObjectName("checkBox_3")
-        ####################################
+
 
         self.horizontalLayout_8.addWidget(self.checkBox_3)
         spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
@@ -58,45 +61,49 @@ class Ui_QSubWindow_data_extraction(object):
         self.verticalLayout_5 = QtWidgets.QVBoxLayout(self.verticalWidget)
         self.verticalLayout_5.setObjectName("verticalLayout_5")
 
-        #### RadioBOX: *.rar Button ####
+        ###
+        # RadioBOX: *.rar Button
+        ###
         # TODO
         #  Write and implement a Method to read this Checkbox and automatically select the *rar Files in Folder, when pressing OK.
         #  I think this has to be done with Signals and in the auxilliary/extract_zip.py, If statement
         self.radioButton_6 = QtWidgets.QRadioButton(self.verticalWidget)
         self.radioButton_6.setObjectName("radioButton_6")
-        ################################
 
         self.verticalLayout_5.addWidget(self.radioButton_6)
 
-        #### RadioBOX: '.zip Button ####
+        ###
+        # RadioBOX: '.zip Button
+        ###
         # TODO
         #  Write and implement a Method to read this Checkbox and automatically select the *zip Files in Folder, when pressing OK.
         #  I think this has to be done with Signals and in the auxilliary/extract_zip.py, If statement
         self.radioButton_5 = QtWidgets.QRadioButton(self.verticalWidget)
         self.radioButton_5.setObjectName("radioButton_5")
-        ################################
 
         self.verticalLayout_5.addWidget(self.radioButton_5)
         self.horizontalLayout_8.addWidget(self.verticalWidget)
         spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_8.addItem(spacerItem2)
 
-        #### CheckBOX: Select Files (Period of time) ####
+        ###
+        # CheckBOX: Select Files (Period of time)
+        ###
         # TODO
         #  Write and implement a Method to read out this Checkbox for the Start Date for a Start Stop Search Function.
         #  I think this has to be done with Signals and in the auxilliary/extract_zip.py, If statement and one additonal Script for pattern Search placed in auililary.<name>
         self.checkBox_4 = QtWidgets.QCheckBox(self.horizontalWidget_8)
         self.checkBox_4.setObjectName("checkBox_4")
-        #################################################
 
         self.horizontalLayout_8.addWidget(self.checkBox_4)
         spacerItem3 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_8.addItem(spacerItem3)
 
-        #### DateEdit Start Date ####
+        ###
+        # DateEdit Start Date
+        ###
         self.dateEdit_3 = QtWidgets.QDateEdit(self.horizontalWidget_8)
         self.dateEdit_3.setObjectName("dateEdit_3")
-        ##################################
 
         self.horizontalLayout_8.addWidget(self.dateEdit_3)
         spacerItem4 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
@@ -107,13 +114,14 @@ class Ui_QSubWindow_data_extraction(object):
         spacerItem5 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_8.addItem(spacerItem5)
 
-        #### DateEdit End Date ####
+        ###
+        # DateEdit End Date
+        ###
         # TODO
         #  Write and implement a Method to read out this Checkbox for the End Date for a Start Stop Search Function.
         #  I think this has to be done with Signals and in the auxilliary/extract_zip.py, If statement and one additonal Script for pattern Search placed in auililary.<name>
         self.dateEdit_4 = QtWidgets.QDateEdit(self.horizontalWidget_8)
         self.dateEdit_4.setObjectName("dateEdit_4")
-        ############################
 
         self.horizontalLayout_8.addWidget(self.dateEdit_4)
         spacerItem6 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
@@ -135,23 +143,25 @@ class Ui_QSubWindow_data_extraction(object):
         spacerItem8 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_9.addItem(spacerItem8)
 
-        #### LineEdit Input Folder ####
+        ###
+        # LineEdit Input Folder
+        ###
         # TODO
         #  Make a New Placeholder, i think it would be more sufficient to Take the Placeholder from auxilliary/read_env.py.
         #  For this there should be a method to check if WorkEnv.xml exists and if not take it from Environment.xy
-
         self.lineEdit_3 = QtWidgets.QLineEdit(self.horizontalWidget_9)
         self.lineEdit_3.setObjectName("lineEdit_3")
         self.lineEdit_3.setPlaceholderText(Environment.wdir)
-        ###############################
 
         self.horizontalLayout_9.addWidget(self.lineEdit_3)
 
-        #### PushButton Browse Button Input Folder ####
+        ###
+        # PushButton Browse Button Input Folder
+        ###
         self.pushButton_3 = QtWidgets.QPushButton(self.horizontalWidget_9)
         self.pushButton_3.setObjectName("pushButton_3")
         self.pushButton_3.clicked.connect(BrowseDialogeInput.browse_function)
-        ###############################################
+
 
         self.horizontalLayout_9.addWidget(self.pushButton_3)
         spacerItem9 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
@@ -164,22 +174,24 @@ class Ui_QSubWindow_data_extraction(object):
         spacerItem10 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_10.addItem(spacerItem10)
 
-        #### LineEdit Output Folder ####
+        ###
+        # LineEdit Output Folder
+        ###
         # TODO
         #  Make a New Placeholder, i think it would be more sufficient to Take the Placeholder from auxilliary/read_env.py.
         #  For this there should be a method to check if WorkEnv.xml exists and if not take it from Environment.xy
         self.lineEdit_4 = QtWidgets.QLineEdit(self.horizontalWidget_10)
         self.lineEdit_4.setObjectName("lineEdit_4")
         self.lineEdit_4.setPlaceholderText(Environment.odir)
-        ################################
 
         self.horizontalLayout_10.addWidget(self.lineEdit_4)
 
-        #### PushButton Browse Button Output Folder ####
+        ###
+        # PushButton Browse Button Output Folder
+        ###
         self.pushButton_4 = QtWidgets.QPushButton(self.horizontalWidget_10)
         self.pushButton_4.setObjectName("pushButton_4")
         self.pushButton_4.clicked.connect(BrowseDialogeOdir.browse_function)
-        ################################################
 
         self.horizontalLayout_10.addWidget(self.pushButton_4)
         spacerItem11 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
@@ -193,30 +205,30 @@ class Ui_QSubWindow_data_extraction(object):
         spacerItem12 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_14.addItem(spacerItem12)
 
-        #### ProgressBar ####
+        ###
+        # ProgressBar
+        ##
         # TODO
         #  Write Class for ProgressBar, with Method to count the Process (count Files/100 refresh ... see google).
         #  Has to be goggled how do to this
-
-        #  Write Function to Bring it to work Count files / 100 .... see google
         self.progressBar_2 = QtWidgets.QProgressBar(self.horizontalWidget_14)
         self.progressBar_2.setProperty("value", 24)
         self.progressBar_2.setObjectName("progressBar_2")
-        ####################
 
         self.horizontalLayout_14.addWidget(self.progressBar_2)
 
-        #### ButtonBox OK, CANCEL ####
-        # TODO
+        ###
+        # ButtonBox OK, CANCEL
+        ###
+        #TODO
         #  Close Window when Cancel is pressedW
-
         self.buttonBox_2 = QtWidgets.QDialogButtonBox(self.horizontalWidget_14)
         self.buttonBox_2.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox_2.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox_2.setObjectName("buttonBox_2")
         # RUN FUNCTION auxiliary.extract_zip.py When Pressing OK
         self.buttonBox_2.accepted.connect(ZippedFiles.unzip_files)
-        ##############################
+
 
         self.horizontalLayout_14.addWidget(self.buttonBox_2)
         spacerItem13 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
@@ -229,8 +241,8 @@ class Ui_QSubWindow_data_extraction(object):
 
     def retranslateUi(self, QSubWindow_data_extraction):
         """
-        This is the Mehode to "retranslate" the Names in the Window.
-        If you want to make Changes to the displayed Names, make it HERE
+        This is the Method to rename every Entry in the Window.
+        If you want to Change Names DO IT HERE
         :param QSubWindow_data_extraction:
         :return:
         """
@@ -256,7 +268,7 @@ class Ui_QSubWindow_data_extraction(object):
 
     def create_Ui_QSubWindow_data_extraction(self):
         """
-        This is the Method to Open the Window from the MainWindow. This has to called from there.
+        This is the Method to Open the QSubWindow from the QMainWindow. This has to called from there.
         :return:
         """
         QSubWindow_data_extraction = QtWidgets.QDialog()
