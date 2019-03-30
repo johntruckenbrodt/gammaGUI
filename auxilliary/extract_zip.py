b04bf88fcee168e6734859d78f938b5f17173154
Matisyahu18 <stefan.werner@uni-jena.de>
2019-03-30 10:02:19 +0100

    made it pretty

--- auxilliary/extract_zip.py
+++ auxilliary/extract_zip.py
@@ -1,35 +1,17 @@
 #!/usr/bin/env python
-
-"""
-Python 2.7.15 |Anaconda, Inc.| (default, May  1 2018, 18:37:09) [MSC v.1500 64 bit (AMD64)]
-Python 2.7.15 |Anaconda, Inc.| (default, May  1 2018, 18:37:09) [MSC v.1500 64 bit (AMD64)] on win32
-
-Tasks: List Zip Files and extract them into Same Folder (GammaGUI needs *.SAFE Structure)
-"""
-
-from auxilliary.environment import Environment
-import xml.etree.cElementTree as ET
 from auxilliary.read_env import ReadEnv
 import os
 import zipfile
 
 class ZippedFiles():
     """
-    This is the Class for Zipped Files
-    At the current Status, this Class contains only one Method to extract *.zip Files (e.g. S1)
-    To initialize the WorkENV a instance of ReadEnv is created. The arguments form the read_env() are return to
-    the Variable my_WorkENV
-    The Class Variables my_WorkENV is a list of the Entries of the WorkEnv.xml (Paths)
-    The order is:
-        0. wdir
-        1. idir
-        2. tdir
-        3. odir
-        4. WorkEnv.xml
-        5. backgroundimage
-        6. GammaCommands.xml
+    This is the Class for Zipped Files. It Contains a Method to read in all *zip Files in one Folder
+    and extract them to the same or another.
     """
     try:
+        """
+        Checks if the WorkENV.xml exists
+        """
 
         WorkENV = ReadEnv()
         my_WorkENV = WorkENV.read_env()
@@ -39,10 +21,11 @@ class ZippedFiles():
 
     def unzip_files():
         """
-        This is the Method of the Class ZippedFiles. It creates a Instance of the Class ZippedFiles.
-        From the returned List, idir and odir are read and passed to the Extraction Loop
-        TODO further TODO's in gammaGUIv2/gui_windows/QSubWindow_data_extraction.py
-        :return:
+        This is the Method to extract Zip Files.
+        Therefore the WorkEnv.xml is read the Folder Path are extracted.
+        After this the Folder is searched for *.zip Files, these are extracted.
+
+        :return: Extracted *zip Files in the Output Path
         """
         WorkENV = ReadEnv()
         args = WorkENV.read_env()
@@ -72,11 +55,6 @@ class ZippedFiles():
         #                                                                      zipfile='zipfile'))  # Keine Print Funktion in Python 3?! für format!?
 
         print('-- Start unzipping --')
-
-        # # Just Printing the WorkENV
-        # for i in range(0,len(args.my_WorkENV)):
-        #     print(args.my_WorkENV[i])
-
         for i in os.listdir(idir):  # loop through items in dir
             print(os.listdir(idir))
             if i.endswith(extension):  # check for ".zip" extension
@@ -86,7 +64,6 @@ class ZippedFiles():
                 zip_ref.extractall(odir)  # extract file to odir
                 zip_ref.close()  # close file
                 print('File: {file} -- successfully extracted --'.format(file=i))
-
         print("-- Unzipping finished --")
 
 
